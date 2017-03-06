d01582785ca2e20511d24f290efa4934bc242ed5
iitjee <iit.jee.help.me@gmail.com>
2017-03-06 22:11:33 +0530

    Create 00 Basics.js

---
+++ Advance/00 Basics.js
@@ -0,0 +1,8 @@
+Node:
+
+Features:
+1. Latest ($node -v  and $node -p "process.versions.v8")
+2. Staged ($node --harmony -p "youCanUseStagedFeaturesInHere"
+3. In progress ($node --v8-options | grep "in progress")
+For eg: see the last feature inProgress which is about trailing commas in function arguments. (a,b,)
+
