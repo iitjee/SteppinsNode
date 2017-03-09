d906646bddd3cba5452f5ac0244ddbc8f0a45449
iitjee <iit.jee.help.me@gmail.com>
2017-03-09 06:53:24 +0530

    Update 00 Basics.js

--- Advance/00 Basics.js
+++ Advance/00 Basics.js
@@ -6,3 +6,8 @@ Features:
 3. In progress ($node --v8-options | grep "in progress")
 For eg: see the last feature inProgress which is about trailing commas in function arguments. (a,b,)
 
+
+
+Event Loop:
+1. ' https://nodejs.org/en/docs/guides/event-loop-timers-and-nexttick/#poll '
+2. ' https://www.youtube.com/watch?v=8aGhZQkoFbQ '
