e83aff038762dd8482e8d4950a07513882e91af2
iitjee <iit.jee.help.me@gmail.com>
2017-03-10 18:54:02 +0530

    Create 01 Improving chat-server.js

---
+++ Advance/03 Networking/01 Improving chat-server.js
@@ -0,0 +1,23 @@
+//right now all messages are echoed to everybody(every socket)
+we dont want to echo to the person who sent the message
+
+socket.on('data', data => {(
+  Object.entries(sockets).forEach(([key, cs]) => { //cs = connected socket
+    if(socket.id == key) return;
+    cs.write(`bla bla`);
+  });  
+});
+
+
+//Let's improve further by connecting to only those sockets which have given their names
+  socket.on('data', data => {(
+    if(!sockets[socket.id]) {
+      socket.name = data.toString().trim();
+      socket.write(`Welcome ${socket.name}! \n`);
+      sockets[socket.id] = socket;
+      return;
+    }
+    Object.entries(...)
+    
+    
+  
