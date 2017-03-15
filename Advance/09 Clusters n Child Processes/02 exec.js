efda099d4b2b2cc91919bd8fbdf8c845131d7aaa
iitjee <iit.jee.help.me@gmail.com>
2017-03-15 05:32:47 +0530

    Update 02 exec.js

--- Advance/09 Clusters n Child Processes/02 exec.js
+++ Advance/09 Clusters n Child Processes/02 exec.js
@@ -3,17 +3,18 @@ By default, the spawn method does not create a shell to execute the command we p
 efficient than the exec method, which does create a shell. The exec method has one other major difference. It buffers the 
 command's generated output and pass the whole value to a callback function. 
 */
-Here's our previous example implemented with an exec method. Since exec uses a shell to execute the command, we can use the shell syntax directly here making use of the shell pipe feature. 
- const {exec} = require('child_process');
- 
- exec('find. -type f | wc -l', (err, stdout, stderr) => {
-   if(err) {
-     console.error(`exec error:  ${err}`);
-     return;
-   }
-   
-   console.log(`Number of files :  ${stdout}`);
- });
+
+//Here's our previous example implemented with an exec method. Since exec uses a shell to execute the command, we can use the shell syntax directly here making use of the shell pipe feature. 
+    const {exec} = require('child_process');
+
+    exec('find. -type f | wc -l', (err, stdout, stderr) => {
+      if(err) {
+        console.error(`exec error:  ${err}`);
+        return;
+      }
+
+      console.log(`Number of files :  ${stdout}`);
+    });
      
 //Exec buffers the output and pass it here in the stdout argument to the callback, which is the output we want to print.
 
@@ -22,12 +23,12 @@ buffer the whole data before it returns it. The spawn function is a much better 
 is big, because that data will be streamed with the standard I/O object and we can actually make the child process inherit the 
 standard io objects of its parents if we want to. 
 
-Here's an example to spawn the same find command, but inherit the process stdin stdout and stderr
-const { spawn } = require('child_process');
+//Here's an example to spawn the same find command, but inherit the process stdin stdout and stderr
+   const { spawn } = require('child_process');
 
-const child = spawn('find', ['.', '-type', 'f'], {
-  stdio: 'inherit'
-});
+   const child = spawn('find', ['.', '-type', 'f'], {
+     stdio: 'inherit'
+   });
 
 So when we execute this command, the data event will be triggered on the process.stdout, making the script output the result 
 right away. If we want to use shell syntax and still get advantage of the streaming of the data that the spawn command give 
