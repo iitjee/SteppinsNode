59223da274bf1303ea0f2ea3fd3d69935a9391db
iitjee <iit.jee.help.me@gmail.com>
2017-03-16 20:44:20 +0530

    Update 00 Basics.js

--- Advance/00 Basics.js
+++ Advance/00 Basics.js
@@ -6,6 +6,14 @@ Features:
 3. In progress ($node --v8-options | grep "in progress")
 For eg: see the last feature inProgress which is about trailing commas in function arguments. (a,b,)
 
+Type the following:
+.break - When in the process of inputting a multi-line expression, entering the .break command (or pressing the <ctrl>-C key combination) will abort further input or processing of that expression.
+.clear - Resets the REPL context to an empty object and clears any multi-line expression currently being input.
+.exit - Close the I/O stream, causing the REPL to exit.
+.help - Show this list of special commands.
+.save - Save the current REPL session to a file: > .save ./file/to/save.js
+.load - Load a file into the current REPL session. > .load ./file/to/load.js
+.editor - Enter editor mode (<ctrl>-D to finish, <ctrl>-C to cancel)
 
 
 Event Loop:
