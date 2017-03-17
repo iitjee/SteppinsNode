8897380ac42f0709745a133240ae8adbed37eb2e
iitjee <iit.jee.help.me@gmail.com>
2017-03-18 04:03:58 +0530

    Update 03 fork.js

--- Advance/09 Clusters n Child Processes/03 fork.js
+++ Advance/09 Clusters n Child Processes/03 fork.js
@@ -26,13 +26,14 @@ parent and forked processes with an interface similar to that of the EventEmitte
       }, 1000);
 
 $node parent.js
+
 When running this code, the forked child will send an incremented counter value every second and the parent will just print 
 that.
 
 
 Let's do a more practical example to using the fork function. Let's say we have an http server that handles multiple 
-endpoints. One of these endpoints is computationally expensive and will take few seconds to complete. I've simulated this with 
-a long for loop here.
+endpoints. One of these endpoints is computationally expensive and will take few seconds to complete. I have simulated 
+this with a long for loop here.
 
 
 
@@ -59,3 +60,52 @@ a long for loop here.
 
       server.listen(3000);
       
+
+When the the /compute endpoint is requested, the server will not be able to handle any other requests because the event loop 
+is busy with the long for loop operation. There are a few ways we can solve this problem depending on the nature of the long 
+operation, but one solution that works for all operations is just to move the computational operation into another process 
+using fork. 
+
+We'll make two files:
+      (server.js)
+            const http = require('http');
+            const fork = require('child_process');
+
+            const server = http.createServer();
+
+            server.on('request', (req, res) => {
+              if(req.url === '/compute') {
+//                 const sum = longComputation();
+//                 return res.end(`Sum is ${sum}`);
+                    const compute = fork('compute.js');
+                    compute.send('start'); //When we get a request, we'll send a message to the forked process to start 
+                                                //processing.
+                    compute.on('message', sum => {//listen to the message event on the forked process itself,
+                          res.end(`response is ${sum}`)'
+              } else {
+                res.end('Ok')
+              }
+            })
+
+            server.listen(3000);
+
+      (compute.js)
+
+            const longComputation = () => {
+              let sum = 0;
+              for(let i=0; i < 1e9; i++) 
+                sum += i;
+
+              return sum;
+            };
+
+            process.on('message', (msg) => {
+                  const sum = longComputation();
+                  process.send(sum);
+            });
+
+
+this code is, of course, limited by the number of processes we can fork, but when we execute this code now and request the 
+long computation over http, the main server is not blocked at all and can take further requests, and when the computation is 
+done, it's received normally as before. Node's cluster module, which we'll explore next, is based on this idea of child 
+process forking and load balancing the requests among the many forks that we can create on any system.
