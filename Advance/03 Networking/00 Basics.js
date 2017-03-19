ca3cf07ab66900e30b338885e97ad7335426c19a
iitjee <iit.jee.help.me@gmail.com>
2017-03-19 21:26:05 +0530

    Update 00 Basics.js

--- Advance/03 Networking/00 Basics.js
+++ Advance/03 Networking/00 Basics.js
@@ -2,7 +2,14 @@ The net module provides you with an asynchronous network wrapper. It contains fu
 (called streams).
 
 (http module vs net module: http://stackoverflow.com/questions/29869999/http-createserver-vs-net-createserver-in-node-js _
-
+net module creates tcp server where http module creates http server.
+ HTTP is on 7th layer of OSI whereas TCP is on 4th layer(Transmission layer). i.e HTTP is built on top of it
+With HTTP server, you can identify whether http headers received are valid or not but you can't do directly with TCP.
+TCP is more like a raw form. On top of it you can make your modules for other protocols like ftp, upd etc.
+This chat server application doesn't need http headers stuff. So we use simple raw TCP protocol :)
+Read the above link for sure!)
+ 
+ 
 const server = require('net').createServer(); //creating server
 
 server.on('connection', socket => { //registering a connection handler (cb)
@@ -16,9 +23,10 @@ server.listen(8000, () => console.log('Server bound')); //listen to the 8000
 //open new terminal and $nc localhost 8000 //use netcat to connect to 8000 port of localhost
 
 //You can type in messages in the nc terminal but we haven't any handler to use the incomeing data to the server form the netcat client
-//inside 'connection' event callback
-  server.on('data', data => {
-    console.log('recevied data is ', data); //you get data in terms of buffer. so user can type in any language
+
+/* Inside 'connection' event callback */
+  socket.on('data', data => {
+    console.log('recevied data is ', data); //you get data in terms of buffer. (eg:  <Buffer 73 64 0a> ) so user can type in any language
     //console.log(`Recieved data is  ${data}`); //you get data like strings.(node assumes utf-8 encoding on the buffers)
    
     socket.write('client side: data is: ');
@@ -27,11 +35,11 @@ server.listen(8000, () => console.log('Server bound')); //listen to the 8000
 
 
 /* we can also set the encoding globally to the socket */
-server.on('connection', socket => { //registering a connection handler (cb)
+server.on('connection', socket => { 
   console.log('connection successful');
   socket.write('Welcome new client'); //socket is duplex stream i.e it is bot readable and writable
   
-  server.on('data', data => {
+  socket.on('data', data => {
     console.log('recevied data is ', data); //you get data in utf8 now
     //console.log(`Recieved data is  ${data}`); //you get data like strings.(node assumes utf-8 encoding on the buffers)
    
@@ -39,12 +47,12 @@ server.on('connection', socket => { //registering a connection handler (cb)
     socket.write(data)
   });
   
-  socet.setEncoding('utf8'); //for this particular socket, all teh streams on this object will assume utf8 standard
+  socket.setEncoding('utf8'); //for this particular socket, all teh streams on this object will assume utf8 standard
 });
 
 
 /* Handling the Disconnect event  */
-socket.on('end', () => {
+server.on('end', () => {
   console.log('Client disonnected');
 }
           
