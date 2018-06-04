b96f475bffc6133f3bcb67b9af7b313c66e60809
iitjee <iit.jee.help.me@gmail.com>
2018-06-04 13:38:11 +0530

    Update 04StaticRoute.js

--- Express/04StaticRoute.js
+++ Express/04StaticRoute.js
@@ -1,6 +1,6 @@
-There will be times that you need to provide static files to your clients; Images, PDFs, downloadable files are all great 
-examples. But the idea of static files doesn't end there. Non dynamic files such as CSS, HTML, and any other static file 
-needed can be hosted this way.
+// There will be times that you need to provide static files to your clients; Images, PDFs, downloadable files are all great 
+// examples. But the idea of static files doesn't end there. Non dynamic files such as CSS, HTML, and any other static file 
+// needed can be hosted this way.
 
 
 //here no path is specified in the first argument. So default / is taken
@@ -9,4 +9,4 @@ needed can be hosted this way.
 //here /imagessss endpoint is specified.     
       app.use('/imagessss', express.static('./public/images'));
 
-note: use either './public/images' or 'public/images' but not '/public/images'
+//note: use either './public/images' or 'public/images' but not '/public/images'
